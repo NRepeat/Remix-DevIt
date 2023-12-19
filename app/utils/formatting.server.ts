f97a9fef378b1afb08428683e375d67a456abd05
Mykyta Nazarov <nnazarov55@gmail.com>
2023-12-19 21:56:43 +0200

    fix window folders names

--- app/utils/formatting.server.ts
+++
@@ -1,9 +0,0 @@
-export function formatString(inputString: string): string {
-  const words = inputString.split("-");
-  const formattedWords = words.map(
-    (word) => word.charAt(0).toUpperCase() + word.slice(1)
-  );
-  const formattedString = formattedWords.join(" ");
-
-  return formattedString;
-}
